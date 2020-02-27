d304dc8d129ef8c04605604c4743539be9d4ed33
Takamasa Fukase <takama224@yahoo.co.jp>
2020-02-28 01:47:47 +0900

    カメラ撮影かライブラリから選ぶかのアクションシートを追加。

--- Camera&PhotoLibraryTest/ViewController.swift
+++ Camera&PhotoLibraryTest/ViewController.swift
@@ -14,7 +14,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
-
+    @IBAction func choosePhotoButton(_ sender: Any) {
+        let actionSheet: UIAlertController = UIAlertController(title: "", message: "アップロードする写真を選択してください", preferredStyle: .actionSheet)
+        let tappedCamera = UIAlertAction(title: "カメラで撮影する", style: .default) { (UIAlertAction) in
+            //何か処理
+        }
+        let tappedLibrary = UIAlertAction(title: "ライブラリから選択する", style: .default) { (UIAlertAction) in
+            //何か処理
+        }
+        let cancel = UIAlertAction(title: "キャンセル", style: .cancel) { (UIAlertAction) in
+            //キャンセル
+        }
+        actionSheet.addAction(tappedCamera)
+        actionSheet.addAction(tappedLibrary)
+        actionSheet.addAction(cancel)
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
 }
 
